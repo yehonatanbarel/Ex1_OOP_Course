2d5d62cdf403b6f3eca3bea3f6c750be304a9d68
yehonatanbarel <79272744+yehonatanbarel@users.noreply.github.com>
2021-11-18 19:02:57 +0200

    Update test_Ex1.py

--- test_Ex1.py
+++ test_Ex1.py
@@ -7,18 +7,16 @@ from Ex1 import Ex1
 
 class Test(TestCase):
     def test_Ex1(self):
-        # create a csv file for the elev calls this is just for the len of the call for line 16
+        # create a csv file for the elev calls this is just for the len of the call for line 15
         e1 = elevator_call('Calls_a.csv')
 
         # this list will have all zero, this is because we want to test that if we have one elev
         # we get all of them to be allocated to elev zero
         expected_lst = [0] * len(e1.src)
-        # we get the csv file from our code and put it in var csv_to_check( i not sure if we get it as a data frame
-        # if i will print line 14 it will give NONE but all the things still run good
+        # we get the csv file from our code and put it in var csv_to_check
         csv_to_check = pd.read_csv('out.csv', header=None)
 
-        # enter all of our allocate that we got from our code into a list actual_lst
-        # with for loop
+        # enter all of our allocate that we got from our code into a list actual_lst with for loop
         actual_lst = []
         for i in range(len(csv_to_check)):
             actual_lst.append(csv_to_check.iloc[i, 5])
